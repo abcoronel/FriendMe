5aebb43bb8327434b533c36768ef3648e5eaccdc
Ana Belen <abcr1968@gmail.com>
2020-04-22 14:22:36 -0400

    Initial Commit

---
+++ FriendMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendMe
+//
+//  Created by Ana Belen Coronel on 4/22/20.
+//  Copyright © 2020 Ana Belen Coronel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
